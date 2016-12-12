115aa8eb90c98b8f7359c9049e62d1cd4ba21fad
Ruslan Rozhkov <bladteam@gmail.com>
2016-12-12 17:11:50 +0200

    Added validation to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,15 +1,18 @@
 class Task < ApplicationRecord
   belongs_to :user
 
+  validates :user_id, presence: true
+  validates :content, presence: true
+
   FORMAT = AutoHtml::Pipeline.new(
       AutoHtml::HtmlEscape.new,
       AutoHtml::YouTube.new(width: '100%', height: 200),
+     # AutoHtml::Instagram.new(width: '100%', height: 200),
       AutoHtml::GoogleMaps.new(width: '100%', height: 200),
-      AutoHtml::Instagram.new(width: '100%', height: 200),
       AutoHtml::Vimeo.new(width: '100%', height: 200),
       AutoHtml::Image.new,
-      AutoHtml::Markdown.new,
       AutoHtml::Link.new(target: '_blank', rel: 'nofollow'),
+      AutoHtml::Markdown.new,
       AutoHtml::SimpleFormat.new
   )
 
